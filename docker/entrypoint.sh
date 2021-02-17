ccbd5fffcdd5c298d6675803e01e7687ec6440cb
chenhung0506 <chenhung0506@gmail.com>
2021-02-17 09:33:54 +0000

    add cron job into container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd /etc/nginx
 rm nginx.conf
-
 set -e
-
 export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
-
-
 envsubst '
 $$NAMESERVER
 ' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+crontab -l | { cat; echo "*/1 * * * * /usr/bin/certbot renew"; } | crontab -
+crond
+
 nginx -t
 nginx -g "daemon off;"
