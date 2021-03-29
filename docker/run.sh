7e0560069dc89bcca668c4be00767bea456f725f
chenhung lin <chenhunglin@chenhungdeMacBook-Pro.local>
2021-03-29 14:27:06 +0800

    add university website

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 TAG=$(docker images | grep reverse-proxy | awk 'NR==1{print$2}')
 echo $TAG
 export TAG=$TAG
-docker-compose -f ./docker-compose.yaml up -d reverse-proxy vote mysql
+docker-compose -f ./docker-compose.yaml up -d reverse-proxy vote mysql university
