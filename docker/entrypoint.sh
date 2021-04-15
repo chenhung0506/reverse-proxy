3cd1a40b10f70316e17ff4579f4e46ce80b3d165
chenhung lin <chenhunglin@chenhungdeMacBook-Pro.local>
2021-04-15 15:42:55 +0800

    add global variable feature

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,13 @@ cd /etc/nginx
 rm nginx.conf
 set -e
 export NAMESERVER=`cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' '`
+
 envsubst '
 $$NAMESERVER
+$$BF_IP
 ' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+cat /etc/nginx/nginx.conf
 
 crontab -l | { cat; echo "0 6 * * * /usr/bin/certbot renew"; } | crontab -
 crond
